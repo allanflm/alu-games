85fdaa356894ec35b2479ec84014498d63bfa026
Allan <allanfelipedk@gmail.com>
2024-06-18 19:30:18 -0300

    feat: installing Hibernate

--- src/main/kotlin/br/com/alura/alugames/dados/Banco.kt
+++ src/main/kotlin/br/com/alura/alugames/dados/Banco.kt
@@ -1,45 +1,22 @@
 package br.com.alura.alugames.dados
 
-import br.com.alura.alugames.modelo.Jogo
-import java.sql.Connection
-import java.sql.DriverManager
-import java.sql.SQLException
+import javax.persistence.EntityManager
+import javax.persistence.EntityManagerFactory
+import javax.persistence.Persistence
 
 object Banco {
-    fun obterConexao(): Connection? {
-        return try {
-            DriverManager.getConnection("jdbc:mysql://localhost:3306/alugames", "Allan", "393106a8")
-        } catch (e: SQLException) {
-            e.printStackTrace()
-            null
-        }
+//    fun obterConexao(): Connection? {
+//        return try {
+//            DriverManager.getConnection("jdbc:mysql://localhost:3306/alugames", "Allan", "393106a8")
+//        } catch (e: SQLException) {
+//            e.printStackTrace()
+//            null
+//        }
+//    }
+
+    fun getEntityManager(): EntityManager {
+        val factory: EntityManagerFactory = Persistence.createEntityManagerFactory("alugames")
+        return factory.createEntityManager()
     }
 
-    fun getJogos(): List<Jogo> {
-        val listaJogos = mutableListOf<Jogo>()
-        val conexao = obterConexao()
-
-        if(conexao != null){
-            try {
-                val statement = conexao.createStatement()
-                val resultado = statement.executeQuery("SELECT * FROM JOGOS")
-
-                while (resultado.next()){
-                    val id = resultado.getInt("id")
-                    val titulo = resultado.getString("titulo")
-                    val capa = resultado.getString("capa")
-                    val descricao = resultado.getString("descricao")
-                    val preco = resultado.getDouble("preco")
-
-                    val jogo = Jogo(titulo, capa,preco,descricao, id)
-                    listaJogos.add(jogo)
-                }
-
-                statement.close()
-            }finally {
-                conexao.close()
-            }
-        }
-        return listaJogos
-    }
 }
