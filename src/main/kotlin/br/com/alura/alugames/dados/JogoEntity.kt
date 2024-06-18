2d87fa4daafff695f3eaf4d40535c1dabd85b844
Allan <allanfelipedk@gmail.com>
2024-06-18 19:59:42 -0300

    feat: Cannot get a connection as the driver manager is not properly initializ

---
+++ src/main/kotlin/br/com/alura/alugames/dados/JogoEntity.kt
@@ -0,0 +1,16 @@
+package br.com.alura.alugames.dados
+
+import javax.persistence.*
+
+@Entity
+@Table(name = "jogos")
+class JogoEntity(
+    val titulo: String = "Titulo do jogo",
+    val capa: String = "Capa do jogo",
+    val preco: Double = 0.0,
+    val descricao: String = "Descricao do jogo",
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Int = 0
+) {
+}
